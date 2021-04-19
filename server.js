f3a8661af790eec30f51c0c061240aa00788f495
Extension1990 <extensiontlhareseng@gmail.com>
2021-04-19 11:30:49 +0200

    skills update

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ const app = express();
 app.use(express.static('./dist/ext-portfolio-website'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/xt-portfolio-website/'}),
+    res.sendFile('index.html', {root: 'dist/ext-portfolio-website/'}),
 );
 
 // Start the app by listening on the default Heroku port
 let port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
+
